2b48fdb9bb6f4958848f5f0e0b4996c3a98f43d4
pedro-cf <pedro_fernandes@live.com>
2024-04-14 21:13:06 +0100

    3.8 python fix

--- stac_fastapi/mongo/basic_auth.py
+++ stac_fastapi/mongo/basic_auth.py
@@ -3,7 +3,7 @@
 import json
 import os
 import secrets
-from typing import Annotated
+from typing_extensions import Annotated
 
 from fastapi import Depends, HTTPException, status
 from fastapi.routing import APIRoute
