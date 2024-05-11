1e66150330e04dc0f0ef1fc192c04d45c160f2b3
jonhealy1 <jonathan.d.healy@gmail.com>
2024-05-11 18:16:19 +0800

    add support for python 3.12

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0/
 
 ## [Unreleased]
 
+### Changed
+
+- Add support for python 3.12.
+
 
 ## [v3.2.1]
 
--- setup.py
+++ setup.py
@@ -43,6 +43,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
     ],
     url="https://github.com/Healy-Hyperspatial/stac-fastapi-mongo",
